99b63bc1bb4d725193770b0a1b4d830d5ba5bf51
KJH <gh7052@gmail.com>
2024-03-21 16:11:10 +0900

    Add : Lv4 Todo 데이터 요소 추가

--- ToDoList/Model/ToDo.swift
+++ ToDoList/Model/ToDo.swift
@@ -5,16 +5,20 @@
 //  Created by 김정호 on 3/19/24.
 //
 
+import Foundation
+
 struct ToDo {
     private static var count = 0
     private var id: Int
     
     var isCompleted: Bool = false
     var title: String
+    let date: Date
     
     init(title: String) {
         Self.count += 1
         self.id = Self.count
         self.title = title
+        self.date = Date()
     }
 }
